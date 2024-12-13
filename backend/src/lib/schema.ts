02323c92a3d63acac27f591a0820d7318b87521b
aqilakmal <faqih.xv@gmail.com>
2024-12-13 09:42:54 +0800

    chore: minor format

--- backend/src/lib/schema.ts
+++ backend/src/lib/schema.ts
@@ -1,9 +1,9 @@
 import { pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
-export const orders = pgTable('orders', {
-  id: text('id').primaryKey(),
-  order_id: varchar('order_id', { length: 256 }).notNull(),
-  name: text('name').notNull(),
-  status: text('status', { enum: ['preparing', 'completed'] }).notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow()
-}); 
+export const orders = pgTable("orders", {
+  id: text("id").primaryKey(),
+  order_id: varchar("order_id", { length: 256 }).notNull(),
+  name: text("name").notNull(),
+  status: text("status", { enum: ["preparing", "completed"] }).notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
